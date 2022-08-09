2b5ec50c93a107c0cabf94f7f0f940a51231990a
niitagile <43024343+niitagile@users.noreply.github.com>
2022-08-09 17:45:54 +0530

    Add files via upload

---
+++ 09Aug/FilterExample2/src/main/java/com/businesslogic/FirstFilter.java
@@ -0,0 +1,56 @@
+package com.businesslogic;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+/**
+ * Servlet Filter implementation class FirstFilter
+ */
+@WebFilter("/FirstServlet")
+public class FirstFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public FirstFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+			PrintWriter out=response.getWriter();
+			String role=request.getParameter("role"); 
+		// pass the request along the filter chain
+		if(role.equals("accountant"))
+		chain.doFilter(request, response);
+		else
+			out.println("you are not allowed to access this page");
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
