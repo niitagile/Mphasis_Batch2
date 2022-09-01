ce4eebbf99e5e4afeb88613c0f200a2a7d5b52db
niitagile <43024343+niitagile@users.noreply.github.com>
2022-09-01 09:49:49 +0000

    Add files via upload

---
+++ 01Sep/UnitTestingProgram/src/pkg9/FooParameterResolver.java
@@ -0,0 +1,22 @@
+package pkg9;
+
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.api.extension.ParameterResolutionException;
+import org.junit.jupiter.api.extension.ParameterResolver;
+
+public class FooParameterResolver implements ParameterResolver {
+
+	@Override
+	public Object resolveParameter(ParameterContext parameterContext,  ExtensionContext extensionContext) throws ParameterResolutionException {
+	    return new Calculation();
+	}
+
+	@Override
+	public boolean supportsParameter(ParameterContext parameterContext,ExtensionContext extensionContext) throws ParameterResolutionException {
+	    return parameterContext.getParameter().getType() == Calculation.class;
+	}
+	 
+ 
+}
+
