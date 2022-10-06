6c0ab47bc33dfecd3e0c9218a764c9d09b78bda9
niitagile <43024343+niitagile@users.noreply.github.com>
2022-10-06 09:29:05 +0000

    Add files via upload

---
+++ 06Oct/TestAnnotationBeforeGroups.java
@@ -0,0 +1,32 @@
+package BAsicAnnotationExamples;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeGroups;
+import org.testng.annotations.Test;
+
+public class TestAnnotationBeforeGroups {
+	
+	
+	MessageUtil messageUtil = new MessageUtil("Test method");
+	@BeforeGroups({"testOne","testTwo"})
+	    public void beforeGroups(){
+	      System.out.println("beforeGroups method executed before test One group");
+	    }
+
+	 @Test(groups={"testOne"})
+	    public void testMethod(){
+	      Assert.assertEquals("Test method", messageUtil.printMessage());
+	    }
+	    
+	 @Test(groups= {"testTwo"})
+	    public void testMethod1() {
+	    	System.out.println("TestMethod2");
+	    }
+	 
+	 @Test(dependsOnGroups = {"testOne"})
+	    public void testMethod2() {
+	    	System.out.println("Test Method3");
+	    }
+
+}
+
